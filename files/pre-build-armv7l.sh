714a1b9faa97a3b71221ae4247dae0e01c840de7
DeFiChain on Raspberry <79512509+Martin8617@users.noreply.github.com>
2022-02-22 14:39:30 +0100

    Update pre-build-armv7l.sh

--- files/pre-build-armv7l.sh
+++ files/pre-build-armv7l.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #!/usr/bin/env fish
 ainVersion=$1
-url="https://github.com/Martin8617/Defi-Node-for-Raspberry-Pi/releases/download/v${ainVersion}/defichain-${ainVersion}-armv7l-raspi.tar.xz"
-outfile="defichain-${ainVersion}-armv7l-raspi.tar.xz";
+url="https://github.com/Martin8617/Defi-Node-for-Raspberry-Pi/releases/download/v${ainVersion}/defichain-${ainVersion}-armv7l-raspi.tar.gz"
+outfile="defichain-${ainVersion}-armv7l-raspi.tar.gz";
 mkdir -p bin
 cd bin && rm -rf win mac linux
 mkdir linux
@@ -15,4 +15,4 @@ tar -xvf "${outfile}"
 cp "defichain-${ainVersion}/bin/defid" .
 cd ../.. && cp temp/linux/defid bin/linux/defid
 rm -rf temp/
-chmod 777 bin/linux/defid
+chmod 0777 bin/linux/defid
